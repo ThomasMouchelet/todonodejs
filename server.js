e8d7cc6eeaf31b6d06f417eddd5a8955cec0d710
Thomas Mouchelet <mouchelet.thomas@gmail.com>
2022-10-10 11:32:26 +0200

    init

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.set('view engine', 'ejs');
+
+const taskList = ["task1", "task2", "task3"];
+
+app.get('/', (req, res) => {
+    res.render("index", {
+        taskList: taskList
+    });
+})
+
+app.get("/add_task", (req, res) => {
+    console.log("add task");
+    console.log(req.query);
+    taskList.push(req.query.taskName);
+    res.redirect("/");
+})
+
+// supprimer une tâche
+
+// modifier une tâche
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
